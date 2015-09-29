ffbed1507a54dc0ba10b0f5e265092472eb0d137
Eric Le Lay <eric.lelay@gmail.com>
2015-09-29 14:35:47 +0200

    chrome extension for hyperionScreen client
    switch ON/OFF hyperionScreen client depending on website netflix / 6
    play / tf1 replay / etc...

---
+++ extension/videoPlayer.js
@@ -0,0 +1,67 @@
+$( document ).ready(function() {
+	$.getJSON(chrome.extension.getURL('/settings.json'), function(settings) {
+		var hyperionReady=false,
+		zones=null,
+		video=null,
+		index=null;
+		
+		function hyperion(params){
+			var verb=null;
+		    if(params){
+		      verbAdaZones='setPassthruOff';
+		      verbHyperion='ffmpegOff'
+		    }else{  
+		      verbAdaZones='setPassthruOn';
+		      verbHyperion='ffmpegOn';
+		    }
+		    for(var i=0; i<index.length; i++){
+		      var cmd={cmd:verbAdaZones, args:[parseInt(index[i])]}
+		      adaZonesClient.emit('cmd', cmd, function(resp, data) {
+		        console.log('server sent resp code ' + resp + 'for zone'+i);
+		      });
+		    }
+		    var cmd={cmd:verbHyperion, args:[]}
+		    hyperionClient.emit('hyperionCmd', cmd, function(resp, data) {
+		      console.log('server sent resp hyperionScreen code ' + resp);
+		    });	
+		}
+	
+		var adaZonesClient = io.connect('http://localhost:'+settings.adaZonesPort+'/adaZones');
+		adaZonesClient.on('connect', function(){
+			console.log('chrome extension adaZonesClient connected')
+		});
+		adaZonesClient.on('zones', function(data) {
+			zones=data;
+			index=[];
+		    for(var i=0; i < zones.length; i++){
+		      index.push(i);
+		    }
+		    hyperion(true)
+			console.log('server sent zones settings');
+		});
+
+		var hyperionClient = io.connect('http://localhost:'+settings.hyperionScreenPort+'/hyperionScreen');
+		hyperionClient.on('connect', function(){
+			hyperionReady =true;
+			console.log('chrome extension hyperionClient connected')
+		});
+		
+		var checkVideo= setInterval(function(){
+			video=$(document).find("video")[0];
+			if(video){ 
+				fireEvents();
+				clearInterval(checkVideo);
+			}
+		},1000);
+		
+		function fireEvents(){
+			$(video).on('play', function(){
+				hyperion(false);
+			})
+			$(video).on('ended', function(){
+				hyperion(true);
+			})
+		}
+	});
+});
+
